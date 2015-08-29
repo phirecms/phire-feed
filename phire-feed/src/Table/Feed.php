1eadaff77171e56e81f6ddbb49a30276c56e4169
Nick Sagona <nick@nolainteractive.com>
2015-08-29 15:38:52 -0500

    Update for pop-db 2.0.1

--- phire-feed/src/Table/Feed.php
+++ phire-feed/src/Table/Feed.php
@@ -11,7 +11,7 @@ class Feed extends Record
      * Table prefix
      * @var string
      */
-    protected static $prefix = DB_PREFIX;
+    protected $prefix = DB_PREFIX;
 
     /**
      * Primary keys
