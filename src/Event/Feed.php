01a6a5708e2bed8a086b716d3743e304d9b440fa
Nick Sagona <nick@nolainteractive.com>
2016-06-18 16:06:35 -0500

    Update event

--- src/Event/Feed.php
+++ src/Event/Feed.php
@@ -86,7 +86,7 @@ class Feed
      */
     public static function getAll(AbstractController $controller, Application $application)
     {
-        if ((!$_POST) && ($controller->hasView()) && (null !== $controller->view()->form) &&
+        if ((!$_POST) && ($controller->hasView()) && (null !== $controller->view()->form) && ($controller->view()->form !== false) &&
             ((int)$controller->view()->form->id != 0) && (null !== $controller->view()->form) &&
             ($controller->view()->form instanceof \Pop\Form\Form)) {
             $type = $controller->view()->form->feed_type;
